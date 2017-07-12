df9eb7d68a86cf738a10f9e94832fd575f46c7e2
AlB <albert.burc@gmail.com>
2017-07-12 17:31:44 -0400

    finito

--- invoice_writer.rb
+++ invoice_writer.rb
@@ -1,50 +1,75 @@
 require 'erb'
 
-kids_data   = File.read('data/kids-data.txt')
-naughty_letter = File.read('templates/invoice_letter_template.txt.erb')
-EXPENSIVE_TOY = 1000
-CHEAP_TOY = 100
+kids_data      = File.read('data/kids-data.txt')
+invoice        = File.read('templates/invoice_sample_letter.txt.erb')
+EXPENSIVE_TOY  = 1000
+CHEAP_TOY      = 100
+FREE_TOY       = 0
+HST            = 0.13
 
 kids_data.each_line do |kid|
 
- kid_data_array = kid.split
+  kid_data_array = kid.split
 
- name          = kid_data_array[0]
- behavior      = kid_data_array[2]
- toys          = kid_data_array[3..8]
- street_number = kid_data_array[9]
- street_name   = kid_data_array[10]
- street_suffix = kid_data_array[11]
- postal_code   = kid_data_array[12]
- house_value   = kid_data_array[13]
+  name          = kid_data_array[0].capitalize
+  behavior      = kid_data_array[2]
+  toys          = kid_data_array[3..8]
+  street_number = kid_data_array[9]
+  street_name   = kid_data_array[10]
+  street_suffix = kid_data_array[11]
+  postal_code   = kid_data_array[12]
+  house_value   = kid_data_array[13].to_i
 
-  nice_kid_total = 0
-if behaviour == 'nice'
-  if house_value > 1000000
-    nice_kid_total = toys * EXPENSIVE_TOY
-  elsif house_value > 200000
-    nice_kid_total = toys * CHEAP_TOY
-  elsif house_value < 200000
-    nice_kid_total = 0
+
+
+#  makes an array of hashes for each kid
+  list_of_toys = [] # use list of toys in the letter erb file
+  toys.each do |toy|
+    toy_hash = {}
+    toy_hash[:name] = toy
+    if toy == "Kaleidoscope"
+      toy_price = FREE_TOY
+    elsif house_value > 1000000
+        toy_price = EXPENSIVE_TOY
+    elsif house_value >= 200000
+        toy_price = CHEAP_TOY
+    elsif house_value < 200000
+      toy_price = FREE_TOY
+    end
+    toy_hash[:price] = toy_price
+    list_of_toys << toy_hash
   end
-elsif behaviour == "naughty"
-  if house_value > 1000000
-    nice_kid_total = EXPENSIVE_TOY
-  elsif house_value > 200000
-    nice_kid_total = CHEAP_TOY
-  elsif house_value < 200000
-    nice_kid_total = 0
+
+  subtotal = 0
+  list_of_toys.each do |toy_hash|
+    toy_price = toy_hash[:price]
+    subtotal += toy_price
   end
-end
-  
 
+  tax = 0
+  list_of_toys.each do |toy_hash|
+    tax = toy_hash[:price] * HST
+  end
 
+  total = 0
+  list_of_toys.each do |toy_hash|
+    toy_price = toy_hash[:price]
+    total = (toy_price * HST) + toy_price
+  end
 
+# subtotal toy prices
 
+# calculate tax
 
+# total toy prices with tax
 
-  filename    = 'letters/invoices' + name + '.txt'
-  letter_text = ERB.new(invoice_letter, nil, '-').result(binding)
+  next unless house_value > 0
+
+  p house_value
+  p list_of_toys
+
+  filename    = 'letters/invoices/' + name + '.txt'
+  letter_text = ERB.new(invoice, nil, '-').result(binding)
 
   puts "Writing #{filename}."
   File.write(filename, letter_text)
